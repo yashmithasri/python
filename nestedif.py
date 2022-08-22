80a3f761e9b58bf51621dc43de302e08e232aebd
yashmithasri <111758506+yashmithasri@users.noreply.github.com>
2022-08-22 19:54:04 +0530

    Create nestedif.py

---
+++ nestedif.py
@@ -0,0 +1,21 @@
+a=int(input("enter 1st number :"))
+b=int(input("enter 2nd number :"))
+c=int(input("enter 3rd number :"))
+if a>b:
+    if a>c:
+        print("a is greater")
+elif b>c:
+    if b>a:
+        print("b is greater")
+else:
+    print("c is greater ")    
+#if a>b:
+#    if a>c: 
+#      print("a ")
+#else:
+#print("c")
+#if b>c:
+#if b>a:
+#print("b")
+#else:
+#print("c")
