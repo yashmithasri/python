66690a5fbb720dd7038f1e9fa2afbe6e408c0ba9
yashmithasri <111758506+yashmithasri@users.noreply.github.com>
2022-08-28 14:25:12 +0530

    Update validationoftriangle.py

--- validationoftriangle.py
+++ validationoftriangle.py
@@ -6,7 +6,9 @@ if sum>180 or sum<180:
     print("triangle not possible")
 elif sum==180 and a==b and b==c:   #we cn also write a=b=c=60:
     print("its a equilateral triangle")
+elif a==b or a==c or b==c and sum==180:
+    print("its iscoseles triangle")
+elif (a==b or a==c or b==c) and (a or b or c ==90)and sum ==180:
+    print("its iscoseles right angled triangle")
 elif a or b or c ==90 and sum==180 :
     print("its right angled")
-elif a==b or a==c or b==c and sum ==180:
-    print("its iscoseles triangle")
