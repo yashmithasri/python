1891b5edec6875c584a8a87fb7d98657ada210ce
yashmithasri <111758506+yashmithasri@users.noreply.github.com>
2022-08-30 11:45:34 +0530

    Update salary calculation.py

--- salary calculation.py
+++ salary calculation.py
@@ -1,18 +1,12 @@
-salary= float(input("salary: "))
-hra = int(input("hra :"))
-da =int(input("da : "))
-if salary ==10000:
-    da = (80/100)*10000
-    hra = (20/100)* da
-    print("da is :"+str(da))
-    print("hra is : "+str(hra))
-elif salary ==20000:
-    da = (90/100)*20000
-    hra = (25/100)*da
-    print("da is "+str(da))
-    print("hra is "+str(hra))
-else:
-    da=(95/100)*salary
-    hra = (30/100)*da
-    print("da is "+str(da))
-    print("hra is "+str(hra))
+salary=float(input("enter basic salary: "))
+if salary<=10000:
+    hra=(salary*20)/100
+    da=(salary*80)/100
+elif salary>10000 and salary<=20000:
+    hra=(salary*25)/100
+    da=(salary*90)/100
+elif salary>20000:
+    hra=(salary*30)/100
+    da=(salary*95)/100
+gross_salary = salary+hra+da
+print(f"gross salary is {gross_salary}")
